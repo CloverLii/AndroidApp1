197be3f8ec614beb9db01a5aff60c81d5a8ac8db
sl302 <sl302@cms-r2-58.cms.waikato.ac.nz>
2019-09-18 16:22:01 +1200

    section 9 ongoing

--- app/src/main/java/comp575/helloworld/Contact.java
+++ app/src/main/java/comp575/helloworld/Contact.java
@@ -1,4 +1,21 @@
 package comp575.helloworld;
 
+import android.os.Parcelable;
+
 public class Contact {
+    public String name;
+    public String email;
+    public String mobile;
+
+    public Contact(String name, String email, String mobile)
+    {
+        this.name = name;
+        this.email = email;
+        this.mobile = mobile;
+    }
+
+    public String toString()
+    {
+        return name;
+    }
 }
--- app/src/main/java/comp575/helloworld/MainActivity.java
+++ app/src/main/java/comp575/helloworld/MainActivity.java
@@ -3,16 +3,41 @@ package comp575.helloworld;
 import android.arch.lifecycle.ViewModelStoreOwner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    private ArrayList<Contact> contacts = new ArrayList<Contact>();
+    private ArrayAdapter<Contact> adapter;
+    private ListView contactListview;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setup adapter
+        contactListview = (ListView)findViewById(R.id.contactsListView);
+
+        adapter = new ArrayAdapter<Contact>(this, android.R.layout.simple_list_item_1,contacts);
+
+        // contactsListView does not exist in portrait view
+        if(contactListview != null){
+            contactListview.setAdapter(adapter);
+            contactListview.setOnItemClickListener(this);
+
+            // Add some contacts
+            contacts.add(new Contact("Joe Bloggs", "joe@bloggs.co.nz", "021123456"));
+            contacts.add(new Contact("Jane Doe", "jane@doe.co.nz", "022123456"));
+        }
+
     }
 
     public void saveContact(View view){
@@ -27,6 +52,15 @@ public class MainActivity extends AppCompatActivity {
         EditText emailField = (EditText) findViewById(R.id.email);
         String email = emailField.getText().toString();
 
-        Toast.makeText(this, "Saved contact for "+ name + email + mobile, Toast.LENGTH_SHORT).show();
+        contacts.add(new Contact(name, email, mobile));
+        //Toast.makeText(this, "Saved contact for "+ name + email + mobile, Toast.LENGTH_SHORT).show();
     }
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Contact contact = (Contact) parent.getAdapter().getItem(position);
+            Toast.makeText(parent.getContext(), "Clicked " + contact,
+                    Toast.LENGTH_SHORT).show();
+            //contacts.add(contact);
+        }
 }
