3ecf0a2c9ace689f0b1a7a5b7669b2ef40d81044
sl302 <sl302@cms-r2-61.cms.waikato.ac.nz>
2019-09-19 15:47:28 +1200

    final version

--- app/src/main/java/comp575/helloworld/Contact.java
+++ app/src/main/java/comp575/helloworld/Contact.java
@@ -1,15 +1,26 @@
 package comp575.helloworld;
 
 
-public class Contact {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Contact implements Parcelable{
     public String name;
     public String email;
     public String mobile;
 
     public Contact(String name, String email, String mobile) {
+        System.out.println(" ******** create Contact with default para");
         this.name = name;
         this.email = email;
         this.mobile = mobile;
+    }
+
+    public Contact(Parcel in) {
+        System.out.println(" ******** create Contact with parcel object");
+        name = in.readString();
+        email = in.readString();
+        mobile = in.readString();
     }
 
     public String toString() {
@@ -21,4 +32,27 @@ public class Contact {
         Contact oldContact = (Contact)obj;
         return this.name.equals(oldContact.name);
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(email);
+        dest.writeString(mobile);
+    }
+
+    public static final Creator<Contact> CREATOR = new Creator<Contact>() {
+        @Override
+        public Contact createFromParcel(Parcel in) {
+            return new Contact(in);
+        }
+        @Override
+        public Contact[] newArray(int size) {
+            return new Contact[size];
+        }
+    };
 }
--- app/src/main/java/comp575/helloworld/MainActivity.java
+++ app/src/main/java/comp575/helloworld/MainActivity.java
@@ -14,10 +14,7 @@ import android.view.View;
 import android.widget.Adapter;
 import java.util.ArrayList;
 
-import junit.framework.Assert;
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private ArrayList<Contact> contacts = new ArrayList<Contact>();
     private ArrayAdapter<Contact> adapter;
@@ -35,15 +32,26 @@ public class MainActivity extends AppCompatActivity {
         //check device orientation
         int orientation = getResources().getConfiguration().orientation;
         if (orientation == Configuration.ORIENTATION_LANDSCAPE){
-            contactListView.setAdapter(adapter);
 
-            // add some Contacts
-            contacts.add(new Contact("Joe Bloggs", "joe@bloggs.co.nz", "021123456"));
-            contacts.add(new Contact("Jane Doe", "jane@doe.co.nz", "022123456"));
+            contactListView.setAdapter(adapter);
+            contactListView.setOnItemClickListener(this);
+
+            // get saved contacts in portrait mode
+            if (savedInstanceState != null) {
+                for (Parcelable contact : savedInstanceState.getParcelableArrayList(
+                        "contacts")) {
+                    contacts.add((Contact) contact);
+                }
+            } else {
+                contacts.add(new Contact("Joe Bloggs", "joe@bloggs.co.nz", "021123456"));
+                contacts.add(new Contact("Jane Doe", "jane@doe.co.nz", "022123456"));
+            }
         }
     }
 
     public void saveContact(View view) {
+
+        // get input values
         EditText nameField = findViewById(R.id.name);
         String name = nameField.getText().toString();
 
@@ -58,17 +66,36 @@ public class MainActivity extends AppCompatActivity {
         // check if new contact exists
         for (Contact c : contacts) {
             if (newContact.equals(c)) {
-                Toast.makeText(this, name + "  already exists", Toast.LENGTH_SHORT).show();
+                System.out.println("******** contact has existed");
+                Toast.makeText(this,  name +" exists, update the info ", Toast.LENGTH_SHORT).show();
+
+                // update the info for exist contact
+                c = newContact;
+                System.out.println("******** update existed contact");
                 return;
             }
         }
-
         // add new contact
+        System.out.print("******** add new contact");
         contacts.add(newContact);
+        Toast.makeText(this, " save new contact successfully", Toast.LENGTH_SHORT).show();
+
 
         // notify adapter change
         if(adapter != null) {
             adapter.notifyDataSetChanged();
         }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Contact contact = (Contact) parent.getAdapter().getItem(position);
+        Toast.makeText(parent.getContext(), "Clicked " + contact, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedState) {
+        savedState.putParcelableArrayList("contacts", contacts);
+        super.onSaveInstanceState(savedState);
     }
 }
